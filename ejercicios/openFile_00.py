8a620a3ecbcb5a9f0ad7ec0ef538579b01982e59
juan Carlos Dergan F <juancarlos.dergan@gmail.com>
2023-04-13 15:16:59 -0500

    Sync Label in simple py

--- ejercicios/openFile_00.py
+++ ejercicios/openFile_00.py
@@ -1,10 +1,10 @@
 filename = './sample_data/california_housing_test.csv'
-fl = open(filename, 'r') # Open the file
+fl = open(filename, 'r')      # Open the file
 linecount = 0
 for line in fl:
     print(line)
     linecount += 1
-    if linecount > 10: break # let's not print too many lines to the console
+    if linecount > 10: break  # let's not print too many lines to the console
 
-fl.close() #close the file 
+fl.close()                    # close the file 
 print("FIN")    
