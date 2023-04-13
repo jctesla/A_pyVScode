985e573af5396db02259c4929e16e6b6cee049b8
juan Carlos Dergan F <juancarlos.dergan@gmail.com>
2023-04-13 15:05:33 -0500

    simple open() file

---
+++ ejercicios/openFile_00.py
@@ -0,0 +1,10 @@
+filename = './sample_data/california_housing_test.csv'
+fl = open(filename, 'r') # Open the file
+linecount = 0
+for line in fl:
+    print(line)
+    linecount += 1
+    if linecount > 10: break # let's not print too many lines to the console
+
+fl.close() #close the file 
+print("FIN")    
