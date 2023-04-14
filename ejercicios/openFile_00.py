b8fb02e168fe857cfb9eba0db0dc338cd2bb3f46
juan Carlos Dergan F <juancarlos.dergan@gmail.com>
2023-04-13 23:46:15 -0500

    formas de open() files

--- ejercicios/openFile_00.py
+++ ejercicios/openFile_00.py
@@ -1,10 +1,26 @@
-filename = './sample_data/california_housing_test.csv'
+filename = 'fileAnyOne.csv'
 fl = open(filename, 'r')      # Open the file
 linecount = 0
 for line in fl:
-    print(line)
+    print(linecount,". ", line)
     linecount += 1
-    if linecount > 10: break  # let's not print too many lines to the console
+    if linecount > 2: break  # let's not print too many lines to the console
 
 fl.close()                    # close the file 
 
+#---------------------------------
+filename = 'fileAnyOne.csv'
+fl = open(filename, 'r') 
+f = True
+while fl and f:
+   f =  fl.readline()
+   print(f)
+
+#--------------------------------
+fl = open(filename, 'r') 
+with fl:
+   print(fl.read())
+
+#--------------------------------
+with open(filename, 'r') as fl:
+  print(fl.read())
