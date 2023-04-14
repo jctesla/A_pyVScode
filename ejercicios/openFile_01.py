d87ba22a5d4c497b2ede7a70a714f097ef49da0a
juan Carlos Dergan F <juancarlos.dergan@gmail.com>
2023-04-14 11:09:59 -0500

    read CSV files

--- ejercicios/openFile_01.py
+++ ejercicios/openFile_01.py
@@ -1,15 +1,19 @@
+# This read the fiel form our local drive, 'fileAnyOne.csv', and separates the fields column for each data.
+
 import csv
 
 filename = 'fileAnyOne.csv'
-row_count = 0
+count = 0
 
-with open(filename, 'r') as fl:
-  csvreader = csv.reader(fl, delimiter=',', quotechar='"')
-  print("Here are CSV contents")
-  for row in csvreader:
-    # only process the first few rows
-    if row_count > 2: break
+with open(filename, 'r') as fl:                                                 # its is the same to assign: fl = open(filename, 'r')
+  csvRead = csv.reader(fl, delimiter=',', quotechar='"')                        # this is the header of the sheet: "longitude","latitude",...
+                                                                                # header content  delimitier=','   &   quotes = "" for each fields. 
+  for row in csvRead:                                                           # loop to read all lines of document.
+     if count > 2: break                                                        # for porpuse demo, only process the first few rows.
+     print(f"Row: {row[0]} | {row[1]} | {row[2]} ... and so on")                # We add demilimiter '|' to the row of lists.
+     count += 1
 
-    # row is a list
-    print("Row: %s | %s | %s ... and so on" % (row[0], row[1], row[2]))
-    row_count += 1
+# out:
+# Row: longitude | latitude | housing_median_age ... and so on
+# Row: -122.050000 | 37.370000 | 27.000000 ... and so on
+# Row: -118.300000 | 34.260000 | 43.000000 ... and so on
